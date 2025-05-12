a664494678c220dda21d4d36ede353692ff8a9f2
Surya <suryakant.v98@gmail.com>
2025-05-12 14:44:25 +0530

    Updated  project section

--- src/app/pages/projects/project-section/project-section.component.ts
+++ src/app/pages/projects/project-section/project-section.component.ts
@@ -7,12 +7,9 @@ import { HeadingColors } from 'src/app/shared/heading/heading-color.model';
   selector: 'app-project-section',
   templateUrl: './project-section.component.html',
   styleUrls: ['./project-section.component.scss'],
-  animations: [
-    fadeInView
-  ]
+  animations: [fadeInView],
 })
 export class ProjectSectionComponent {
-
   @ViewChildren(ProjectCardComponent, { read: ElementRef })
   projs!: QueryList<ElementRef>;
   inViewList: Array<boolean> = [];
@@ -20,17 +17,24 @@ export class ProjectSectionComponent {
 
   projects: Array<any> = [
     {
+      projectImg: 'intel.png',
+      projectName: 'Intel - HSDES',
+      projectDescription: 'It is an High Speed Database - Enterprise System. This enables cross product teams from design to execution. A mission-critical data cloud for Intel Product design, Development, Validation, Execution, Defect Management and much more.',
+      softwaresUsed: 'HTML, SCSS, Bootstrap, Typescript, Angular',
+      softwareAvailable: true,
+    },
+    {
       projectImg: 'vorwerk.png',
       projectName: 'Vorwerk DS Pro',
       projectDescription: 'It is an MLM (Multi-Level Marketing) application that is a software platform designed to support the operations of multi-level marketing businesses. It provides distributors with tools to manage their downline, track sales, and commissions, access product catalogs, communicate with team members, and access training resources.',
-      softwaresUsed: 'HTML, CSS, Bootstrap, Typescript, Angular, AG-grid',
+      softwaresUsed: 'HTML, SCSS, Bootstrap, Typescript, Angular, AG-grid',
       softwareAvailable: true,
     },
     {
       projectImg: 'dsi.png',
       projectName: 'Discern Science Avatar',
-      projectDescription: 'AVATAR (Automated Virtual Agent for Truth Assessments in Real-Time) is fitted with proprietary algorithms to process and analyze the complex signals generated during an automated interview. Like other Artificial Intelligence systems, the AVATAR can be trained to learn through feedback.',
-      softwaresUsed: 'HTML, CSS, Bootstrap, Typescript, Angular',
+      projectDescription:'AVATAR (Automated Virtual Agent for Truth Assessments in Real-Time) is fitted with proprietary algorithms to process and analyze the complex signals generated during an automated interview. Like other Artificial Intelligence systems, the AVATAR can be trained to learn through feedback.',
+      softwaresUsed: 'HTML, SCSS, Bootstrap, Typescript, Angular',
       softwareAvailable: true,
     },
     {
@@ -41,19 +45,17 @@ export class ProjectSectionComponent {
       projectGithubLink: 'https://github.com/Suryakant-98/Lang-Translation-Library',
       isProjectGithubLinkPresent: true,
       projectDescription: 'The Translation Library is a robust solution for handling language localization and internationalization (i18n) in Angular applications. It streamlines the process of managing translations for different languages, making it easier to create multilingual user interfaces.',
-      softwareAvailable: false
-    }
-  ]
+      softwareAvailable: false,
+    },
+  ];
 
   @HostListener('window:scroll')
   checkScroll() {
     const scrollPosition = window.pageYOffset + window.innerHeight;
-    
     this.projs.forEach((v, index) => {
       if (index && v && v.nativeElement.offsetTop <= scrollPosition) {
         this.inViewList[index] = true;
       }
-    })
+    });
   }
-
 }
